747da266e63e873d223182cf4f8517734bfe4003
David <minulin.david@mail.ru>
2022-06-11 12:47:23 +0500

    Поправки в файл мейн.джс

--- js/main.js
+++ js/main.js
@@ -1,36 +1,40 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно. Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random.
 const getRandomInt = (min, max) => {
-   min = Math.ceil(min);
-   max = Math.floor(max);
-   if (min < 0) {
-     return console.log('Пожалуйста, введите положительное число от нуля включительно');
-   }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if (min < 0) {
+    return 'Пожалуйста, введите положительное число от нуля включительно';
+  }
 
-   if (min > max) {
-     return console.log('Вы написали числа диапазона наоборот. Поменяйте значения друг с другом.');
-   }
+  if (min > max) {
+    return 'Вы написали числа диапазона наоборот. Поменяйте значения друг с другом.';
+  }
 
-   if (min === max) {
-     return console.log('Вы написали одинаковые числа диапазона. Пожалуйста, задайте разные значения.');
-   }
+  if (min === max) {
+    return 'Вы написали одинаковые числа диапазона. Пожалуйста, задайте разные значения.';
+  }
 
-   return Math.floor(Math.random() * (max - min + 1)) + min;
- }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
- // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно. Источник: https://learn.javascript.ru/number.
- const getRandomFractional = (min, max, demicalPlaces) => {
- if (min < 0) {
-   return console.log('Пожалуйста, введите положительное число от нуля включительно');
- }
+getRandomInt(0, 5);
 
- if (min > max) {
-   return console.log('Вы написали числа диапазона наоборот. Поменяйте значения друг с другом.');
- }
+// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно. Источник: https://learn.javascript.ru/number.
+const getRandomFractional = (min, max, demicalPlaces) => {
+  if (min < 0) {
+    return 'Пожалуйста, введите положительное число от нуля включительно';
+  }
 
- if (min === max) {
-   return console.log('Вы написали одинаковые числа диапазона. Пожалуйста, задайте разные значения.');
- }
+  if (min > max) {
+    return 'Вы написали числа диапазона наоборот. Поменяйте значения друг с другом.';
+  }
 
- let signs = Math.random() * (max - min + 1) + min;
- return Number(signs.toFixed(demicalPlaces));
- }
+  if (min === max) {
+    return 'Вы написали одинаковые числа диапазона. Пожалуйста, задайте разные значения.';
+  }
+
+  const signs = Math.random() * (max - min + 1) + min;
+  return Number(signs.toFixed(demicalPlaces));
+};
+
+getRandomFractional(2, 10);
