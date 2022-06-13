f68338334ead8636e845e53a6bb7b9e686fafc5a
David <minulin.david@mail.ru>
2022-06-13 13:16:51 +0500

    Исправление замечаний

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно. Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random.
 const getRandomInt = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
+  minInt = Math.ceil(min);
+  maxInt = Math.floor(max);
   if (min < 0) {
     return 'Пожалуйста, введите положительное число от нуля включительно';
   }
@@ -14,13 +14,13 @@ const getRandomInt = (min, max) => {
     return 'Вы написали одинаковые числа диапазона. Пожалуйста, задайте разные значения.';
   }
 
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (maxInt - minInt + 1)) + minInt;
 };
 
 getRandomInt(0, 5);
 
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно. Источник: https://learn.javascript.ru/number.
-const getRandomFractional = (min, max, demicalPlaces) => {
+const getRandomFloat = (min, max, decimalPlaces) => {
   if (min < 0) {
     return 'Пожалуйста, введите положительное число от нуля включительно';
   }
@@ -33,8 +33,8 @@ const getRandomFractional = (min, max, demicalPlaces) => {
     return 'Вы написали одинаковые числа диапазона. Пожалуйста, задайте разные значения.';
   }
 
-  const signs = Math.random() * (max - min + 1) + min;
-  return Number(signs.toFixed(demicalPlaces));
+  const randomNumber = Math.random() * (max - min + 1) + min;
+  return Number(randomNumber.toFixed(decimalPlaces));
 };
 
-getRandomFractional(2, 10);
+getRandomFloat(2, 10, 5);
