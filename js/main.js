9d74843c9dc28de38491e2a968d8234045af0088
David <minulin.david@mail.ru>
2022-06-13 19:49:08 +0500

    поправки в объявлении переменных

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно. Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random.
 const getRandomInt = (min, max) => {
-  minInt = Math.ceil(min);
-  maxInt = Math.floor(max);
-  if (min < 0) {
+  const minInt = Math.ceil(min);
+  const maxInt = Math.floor(max);
+  if (minInt < 0) {
     return 'Пожалуйста, введите положительное число от нуля включительно';
   }
 
-  if (min > max) {
+  if (minInt > maxInt) {
     return 'Вы написали числа диапазона наоборот. Поменяйте значения друг с другом.';
   }
 
-  if (min === max) {
+  if (minInt === maxInt) {
     return 'Вы написали одинаковые числа диапазона. Пожалуйста, задайте разные значения.';
   }
 
   return Math.floor(Math.random() * (maxInt - minInt + 1)) + minInt;
 };
 
-getRandomInt(0, 5);
+getRandomInt(0, 10);
 
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно. Источник: https://learn.javascript.ru/number.
 const getRandomFloat = (min, max, decimalPlaces) => {
